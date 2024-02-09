a3d3e1f42d54ceac630b39f658969298fb045dd3
ishuduwal <ishuduwal8@gmail.com>
2024-02-09 23:29:54 +0545

    signup done local storage left..

--- client/src/api/User.js
+++ client/src/api/User.js
@@ -2,5 +2,5 @@ import axios from "axios";
 const URL = "http://localhost:5000/user"
 
 export const GetUser = () => axios.get(URL); 
-export const Signup = (user)=> axios.post(`${URL}/signup`,user)
-export const Login = (user)=> axios.post(`${URL}/login`,user);
+export const SignupUser = (user)=> axios.post(`${URL}/signup`,user)
+export const LoginUser = (user)=> axios.post(`${URL}/login`,user);
--- client/src/components/user/Signup.jsx
+++ client/src/components/user/Signup.jsx
@@ -1,37 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { SignupUser } from '../../api/User';
 
 export const Signup = () => {
     const navigate = useNavigate();
-  
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser({ ...user, [name]: value });
+    };
+
+    const SignupHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await SignupUser(user);
+            const userData = response.data;
+
+            localStorage.setItem('user', JSON.stringify(userData));
+
+            navigate('/'); // Redirect to home page after successful signup
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     const navigateLogin = () => {
-      navigate('/login')
-    }
-  return (
-    <>
-    <div className='signup'>
-    <div className='login-form'>
-          <h2>Signup</h2>
-          <form>
-            <div>
-              <label>Username</label>
-              <input type='text' />
+        navigate('/login');
+    };
+
+    return (
+        <>
+            <div className='signup'>
+                <div className='login-form'>
+                    <h2>Signup</h2>
+                    <form>
+                        <div>
+                            <label>Username</label>
+                            <input type='text' name='username' value={user.username} required onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input type='email' name='email' value={user.email} required onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input type='password' name='password' value={user.password} required onChange={handleChange} />
+                        </div>
+                        <div>
+                            <button onClick={SignupHandler}>Signup</button>
+                        </div>
+                        <p>Already have an account?<span onClick={navigateLogin}>Login</span></p>
+                    </form>
+                </div>
             </div>
-            <div>
-              <label>Email</label>
-              <input type='email' />
-            </div>
-            <div>
-              <label>Password</label>
-              <input type='password'/>
-            </div>
-            <div>
-              <button>Login</button>
-            </div>
-            <p>Already have an account<span onClick={navigateLogin}>Login</span></p>
-          </form>
-    </div>
-    </div>
-    </>
-  )
-}
+        </>
+    );
+};
